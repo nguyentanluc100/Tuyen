1da550a097833bed0f81b36b681a752d75544bd0
nguyentanluc100 <69811493+nguyentanluc100@users.noreply.github.com>
2022-04-30 00:44:34 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Tuyền';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100005930959050';
+const eventName = 'Happy Birthday';
+const titleCard = '10-5';
+const contentCard = 'Sending you smiles for every moment of your special day. Have a wonderful time and a very happy birthday!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'Tuyen.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
